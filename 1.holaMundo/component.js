4e11ef3fdb7b244520b3c3985fd351bb192081e7
Jon Ander Ochoa <jonanderochoa@gmail.com>
2021-08-04 22:54:21 +0200

    1º Componente
    Modificando texto usando un atributo

--- 1.holaMundo/component.js
+++ 1.holaMundo/component.js
@@ -2,13 +2,27 @@ class HelloWorldComponent extends HTMLElement {
 
     constructor(){
         super();
-        this.saludo = "Hola Mundo";
+        this.saludo = "Hola Mundo, como estas";
+    }
+
+    // Se dispara cuando se cambia un atributo
+    // Se dispara antes del connectedCallback
+    attributeChangedCallback(atributo, viejoValor, nuevoValor) {
+        console.log(`El atributo ${atributo} ha cambiado de ${viejoValor} a ${nuevoValor}`);
+        if(atributo === 'nombre') {
+            this.saludo = `Hola Mundo, como estas ${nuevoValor}`;
+        }
+    }
+
+    // Lista de atributos que queremos observar
+    static get observedAttributes() {
+        return ['nombre'];
     }
 
     // Se ejecuta cuando se añade el componente a un DOM
     connectedCallback() {
         console.log("Añadido al DOM");
-        
+
         let $saludo = document.createElement("strong");
         $saludo.innerHTML = this.saludo;
         this.appendChild($saludo);
